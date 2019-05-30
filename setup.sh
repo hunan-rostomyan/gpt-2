ca2274baf6ff469ab232b75dcc1f5e3d49222236
Hunan Rostomyan <hrostomyan@chegg.com>
2019-05-30 14:30:29 -0700

    Add instruction to set up 345M binary

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ aws s3 sync s3://hunan-gpt2-artifacts/ s3
 
 cp -r s3/models/345M venv/lib/python3.6/site-packages/pytorch_pretrained_bert/
 cp s3/config.json venv/lib/python3.6/site-packages/pytorch_pretrained_bert/345M
+cp s3/pytorch_model.bin venv/lib/python3.6/site-packages/pytorch_pretrained_bert/345M
 
 # patch for v0.6.2: https://github.com/huggingface/pytorch-pretrained-BERT/issues/537
 # the next release should include the fix
